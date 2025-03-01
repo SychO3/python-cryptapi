0b196ac918c3082cc57529d277d15c5223464170
SychO <vbdd@msn.com>
2025-03-01 16:21:18 +0800

    Add synchronous and asynchronous tests for CryptAPIHelper and AsyncCryptAPIHelper

---
+++ cryptapi/exceptions.py
@@ -0,0 +1,14 @@
+"""
+CryptAPI's Python Helper Exceptions
+
+This module provides exception classes for CryptAPI Python helper.
+"""
+
+
+class CryptAPIException(Exception):
+    """
+    Exception raised for CryptAPI-specific errors.
+    Used when the API returns an error response.
+    """
+
+    pass
